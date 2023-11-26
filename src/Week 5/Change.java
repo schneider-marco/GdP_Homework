f697462706547f7fdaac8bc323e1f43981cb455f
masc <masc@masc.de>
2023-11-26 23:15:55 +0100

    Commit

--- src/Week 5/Change.java
+++ src/Week 5/Change.java
@@ -7,15 +7,17 @@ public class Change {
         // start
         setExchangeRates(new int[]{10, 5, 2, 10});
         printMoney(Change.exchangeRates);
-        System.out.println(getExchangeRate(4, 2));
+        System.out.println(getExchangeRate(0, 0));
         System.out.println(Change.convertToCheapestCurrency(new int[]{0, 0, 1, 3, 1}));
+        System.out.println(Arrays.toString(convertToCurrencies(1280)));
+        System.out.println(Arrays.toString(computeChange(new int[]{0, 0, 1, 3, 1}, new int[]{0, 1})));
     }
 
     /**
      *
      * @param arr Integer Array mit den entsprechenden Wechselkursen
-     *
-     */
+     * Speicehr die Wechselkurse in exchangeRates
+     * */
     public static void setExchangeRates(int[] arr) {
         exchangeRates = new int[arr.length];
         for (int i = 0; i < arr.length; i++){
@@ -24,10 +26,20 @@ public class Change {
 
     }
 
+    /**
+     *
+     * @param arr Integer Array, welches, auf der Konsole ausgegeben werden soll
+     */
     public static void printMoney(int[] arr) {
         System.out.println(Arrays.toString(arr));
     }
 
+    /**
+     *
+     * @param from Aktuelle Waehrung
+     * @param to Waehrung, in welche das Geld umgetauscht werden soll
+     * @return Gibt den Umrechnungsfaktor zurueck
+     */
     public static float getExchangeRate(int from, int to) {
 
         float[][] rate = new float[exchangeRates.length+1][exchangeRates.length+1];
@@ -64,6 +76,11 @@ public class Change {
         }
     }
 
+    /**
+     *
+     * @param arr Die Methode bekommt ein Ganzzahl-Array übergeben, in dem für die Währungsarten jeweils eine Anzahl an Münzen dieser Währung gespeichert ist
+     * @return Alle verfügbaren Münzen in Münzen der geringsten Währung umwandeln und zurückgeben
+     */
     public static int convertToCheapestCurrency (int[] arr){
         if (arr.length > exchangeRates.length+1){
             return 0;
@@ -79,8 +96,89 @@ public class Change {
         }
     }
 
+    /**
+     *
+     * @param coins  Anzahl an Münzen der geringsten Währung
+     * @return Gibt ein Array zurück, das diese Münzen so weit wie möglich in die höherwertigen Währungen umrechnet
+     */
     public static int[] convertToCurrencies (int coins){
-        return
+
+
+        int newcoins = coins;
+        int[] arr = new int[5];
+        for (int i = 5; i >= 0; i--) {
+
+            switch (i) {
+                case 4 -> {
+                    newcoins = coins % 1000;
+                    arr[i] = (int) ((coins-newcoins) * getExchangeRate(0, i));
+                    coins = newcoins;
+                }
+                case 3 -> {
+                    newcoins = coins % 100;
+                    arr[i] = (int) ((coins-newcoins) * getExchangeRate(0, i));
+                    coins = newcoins;
+                }
+                case 2 -> {
+                    newcoins = coins % 50;
+                    arr[i] = (int) ((coins-newcoins) * getExchangeRate(0, i));
+                    coins = newcoins;
+                }
+                case 1 -> {
+                    newcoins = coins % 10;
+                    arr[i] = (int) ((coins-newcoins) * getExchangeRate(0, i));
+                    coins = newcoins;
+                }
+                case 0 -> {
+                    arr[i] = coins;
+                }
+            }
+
+        }
+        int lenght = 0;
+
+        for (int i = arr.length-1; i >= 0; i--) {
+            if (arr[i] == 0){
+                continue;
+            }
+            lenght = i;
+            break;
+        }
+
+        int[] arr2 = new int[lenght+1];
+        for (int i = 0; i < arr2.length; i++){
+            arr2[i] = arr[i];
+        }
+
+        return arr2;
+    }
+
+    /**
+     *
+     * @param costs Kosten eines Produkts
+     * @param paid gezahlter Betrag
+     * @return Gibt das Geld zurück, welches nach dem Kauf uebrig bleibt
+     */
+    public static int[] computeChange(int[] costs, int[] paid) {
+
+        int cost = 0;
+        int pay = 0;
+
+        for (int i = 0; i < costs.length; i++) {
+            cost = (int) (cost + (costs[i] * getExchangeRate(i, 0)));
+        }
+
+        for (int i = 0; i < paid.length; i++) {
+            pay = (int) (pay + (paid[i] * getExchangeRate(i, 0)));
+        }
+
+        int total = pay - cost;
+        if (total < 0){
+            return new int[]{};
+        }else {
+            return convertToCurrencies(total);
+        }
+
     }
 
 }
